6066b2020f26574e2502ad13ad8b047dc75317db
tahfeezmizan <mizanahmed5316@gmail.com>
2026-03-04 01:15:25 +0600

    create login page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import { LoginScreen } from "@/components/auth/LoginScreen";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <LoginScreen />
+    </div>
+  );
+}
