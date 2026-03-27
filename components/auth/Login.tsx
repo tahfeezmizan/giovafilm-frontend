701449fc1d44fdfa7ca90ad3802182adbabc8bfe
tahfeezmizan <mizanahmed5316@gmail.com>
2026-03-27 06:24:41 +0600

    udpate login page, auth layout, added white logo

--- components/auth/Login.tsx
+++ components/auth/Login.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Lock, Mail } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
 
@@ -10,17 +9,15 @@ import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 
+
+
 type FormValues = {
   email: string;
   password: string;
 };
 
 export const Login = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormValues>();
+  const { register, handleSubmit } = useForm<FormValues>();
 
   const onSubmit = (data: FormValues) => {
     console.log("Form Data:", data);
@@ -30,24 +27,10 @@ export const Login = () => {
     <div className="flex flex-col justify-center min-h-screen">
       {/* Welcome Text */}
       <div className="mb-8">
-        <h2 className="text-2xl font-bold text-[#1A1A1A] mb-2">
-          Create an account
-        </h2>
+        <h2 className="text-2xl font-bold text-[#1A1A1A] mb-2">Welcome Back</h2>
         <p className="text-sm text-[#757575]">
-          Welcome to the community! Join us and start your adventure.
+          Sign in to access your road trip plans and community.
         </p>
-      </div>
-
-      {/* Social Login */}
-      <SocialLogin />
-
-      {/* Divider */}
-      <div className="w-full flex items-center my-8">
-        <div className="flex-1 h-px bg-[#EEEEEE]"></div>
-        <span className="px-4 text-[10px] font-medium text-[#9E9E9E] uppercase tracking-wider">
-          Or continue with
-        </span>
-        <div className="flex-1 h-px bg-[#EEEEEE]"></div>
       </div>
 
       {/* Form */}
@@ -63,7 +46,7 @@ export const Login = () => {
               type="email"
               placeholder="your@email.com"
               {...register("email")}
-              className="w-full pl-12 pr-4 py-4 bg-white border border-[#E0E0E0] rounded-2xl text-sm focus-visible:ring-2 focus-visible:ring-[#FFC107] focus-visible:border-transparent transition-all"
+              className="w-full pl-12 pr-4 py-6  bg-gray-100/80 border border-[#E0E0E0] rounded-lg focus-visible:ring-2 focus-visible:ring-[#FFC107] focus-visible:border-transparent transition-all shadow-none"
             />
           </div>
         </div>
@@ -79,7 +62,7 @@ export const Login = () => {
               type="password"
               placeholder="••••••••"
               {...register("password")}
-              className="w-full pl-12 pr-4 py-4 bg-white border border-[#E0E0E0] rounded-2xl text-sm focus-visible:ring-2 focus-visible:ring-[#FFC107] focus-visible:border-transparent transition-all"
+              className="w-full pl-12 pr-4 py-6  bg-gray-100/80 border border-[#E0E0E0] rounded-lg focus-visible:ring-2 focus-visible:ring-[#FFC107] focus-visible:border-transparent transition-all shadow-none"
             />
           </div>
         </div>
@@ -88,7 +71,7 @@ export const Login = () => {
         <div className="text-right">
           <Link
             href={"/"}
-            className="text-xs font-bold text-[#424242] cursor-pointer"
+            className="font-semibold text-primary font-public-sans cursor-pointer hover:underline"
           >
             Forgot Password?
           </Link>
@@ -97,17 +80,32 @@ export const Login = () => {
         {/* Login Button */}
         <Button
           type="submit"
-          className="w-full py-4 bg-[#FFC107] text-white font-bold rounded-2xl shadow-[0_8px_20px_rgba(255,193,7,0.3)] hover:bg-[#FFB300] transition-all mt-4"
+          className="w-full bg-[#FFC107] hover:bg-[#FFB300] text-black font-bold rounded-lg px-10 h-14 text-base shadow-lg shadow-yellow-500/20"
         >
           Login
         </Button>
       </form>
 
+      {/* Divider */}
+      <div className="w-full flex items-center my-8">
+        <div className="flex-1 h-px bg-[#EEEEEE]"></div>
+        <span className="px-4 text-[10px] font-medium text-[#9E9E9E] uppercase tracking-wider">
+          Or continue with
+        </span>
+        <div className="flex-1 h-px bg-[#EEEEEE]"></div>
+      </div>
+
+      {/* Social Login */}
+      <SocialLogin />
+
       {/* Footer */}
-      <div className="text-sm">
+      <div className="text-sm text-center mt-6">
         <span className="text-[#0A0A0A]">Don't have an account? </span>
-        <Link href="/signup" className="font-bold text-[#1A1A1A]">
-          Sign Up
+        <Link
+          href="/signup"
+          className="text-base font-semibold text-primary font-public-sans cursor-pointer hover:underline"
+        >
+          Create an account
         </Link>
       </div>
     </div>
--- components/auth/auth-layout.tsx
+++ components/auth/auth-layout.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import type { ReactNode } from "react";
 
 import registerImg from "@/public/auth/register.png";
+import Link from "next/link";
 
 interface AuthLayoutProps {
   children: ReactNode;
@@ -10,15 +11,37 @@ interface AuthLayoutProps {
 export function AuthLayout({ children }: AuthLayoutProps) {
   return (
     <div className="min-h-screen flex">
-      <div className="hidden lg:flex lg:w-1/2 relative">
-        <Image
-          src={registerImg}
-          alt="Authentication background"
-          fill
-          className="object-cover"
-          priority
-        />
-        <div className="absolute inset-0 bg-black/20" />
+      <div
+        className="hidden lg:flex lg:w-1/2 bg-cover bg-center min-h-screen"
+        style={{
+          backgroundImage: `
+      linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)),
+      url(${registerImg.src})
+    `,
+        }}
+      >
+        <div className="flex flex-col justify-center space-y-6 ml-28 text-white">
+          {/* Logo */}
+          <div className="shrink-0">
+            <Link href="/">
+              <Image
+                src={require("@/public/white-logo.png")}
+                alt="Dashboard Logo"
+                height={200}
+                width={200}
+                className="w-48 h-auto"
+              />
+            </Link>
+          </div>
+
+          <h2 className="text-5xl font-black leading-14 font-public-sans">
+            Start Your Journey <br /> With Us Today.
+          </h2>
+          <p className="text-xl font-public-sans ">
+            Join over 50,000 travelers exploring the world's <br /> most scenic
+            routes and hidden gems.
+          </p>
+        </div>
       </div>
 
       <div className="flex-1 lg:w-1/2 flex items-center justify-center p-8">
